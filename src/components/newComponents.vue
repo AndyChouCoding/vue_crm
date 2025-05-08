b784188b90f91c493983c12f84ebaa36a7735dca
AndyChouCoding <andychouforwk@gmail.com>
2025-05-08 16:05:36 +0800

    create componets page

--- src/components/newComponents.vue
+++ src/components/newComponents.vue
@@ -1,3 +1,331 @@
 <template>
-    <div>newComponents</div>
+  <div>
+    <div class="flex flex-col items-center">
+      <div class="flex justify-between">
+        <div class="">
+          <div
+            class="w-[400px] bg-[#324053] text-white font-bold text-center p-2 rounded-xl m-2"
+          >
+            授權頁面Approved Page
+          </div>
+          <div class="flex">
+            <div class="w-[200px] mx-2">
+              <div
+                class="bg-gradient-to-r from-[#f19851] via-[#e95075] via-[#cf58ae] via-[#8667f4] via-[#6e76fa] to-[#3ba2fc] text-white text-center font-bold p-2 rounded-xl"
+              >
+                自訂 CSS 樣式
+              </div>
+              <div class="bg-gray-200 mt-2 rounded-xl">
+                <div class="flex justify-around pt-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-line text-green-500"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M8 0c4.411 0 8 2.912 8 6.492 0 1.433-.555 2.723-1.715 3.994-1.678 1.932-5.431 4.285-6.285 4.645-.83.35-.734-.197-.696-.413l.003-.018.114-.685c.027-.204.055-.521-.026-.723-.09-.223-.444-.339-.704-.395C2.846 12.39 0 9.701 0 6.492 0 2.912 3.59 0 8 0M5.022 7.686H3.497V4.918a.156.156 0 0 0-.155-.156H2.78a.156.156 0 0 0-.156.156v3.486c0 .041.017.08.044.107v.001l.002.002.002.002a.15.15 0 0 0 .108.043h2.242c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157m.791-2.924a.156.156 0 0 0-.156.156v3.486c0 .086.07.155.156.155h.562c.086 0 .155-.07.155-.155V4.918a.156.156 0 0 0-.155-.156zm3.863 0a.156.156 0 0 0-.156.156v2.07L7.923 4.832l-.013-.015v-.001l-.01-.01-.003-.003-.011-.009h-.001L7.88 4.79l-.003-.002-.005-.003-.008-.005h-.002l-.003-.002-.01-.004-.004-.002-.01-.003h-.002l-.003-.001-.009-.002h-.006l-.003-.001h-.004l-.002-.001h-.574a.156.156 0 0 0-.156.155v3.486c0 .086.07.155.156.155h.56c.087 0 .157-.07.157-.155v-2.07l1.6 2.16a.2.2 0 0 0 .039.038l.001.001.01.006.004.002.008.004.007.003.005.002.01.003h.003a.2.2 0 0 0 .04.006h.56c.087 0 .157-.07.157-.155V4.918a.156.156 0 0 0-.156-.156zm3.815.717v-.56a.156.156 0 0 0-.155-.157h-2.242a.16.16 0 0 0-.108.044h-.001l-.001.002-.002.003a.16.16 0 0 0-.044.107v3.486c0 .041.017.08.044.107l.002.003.002.002a.16.16 0 0 0 .108.043h2.242c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157H11.81v-.589h1.525c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157H11.81v-.589h1.525c.086 0 .155-.07.155-.156Z"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-google"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-messenger text-[#805dde]"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M0 7.76C0 3.301 3.493 0 8 0s8 3.301 8 7.76-3.493 7.76-8 7.76c-.81 0-1.586-.107-2.316-.307a.64.64 0 0 0-.427.03l-1.588.702a.64.64 0 0 1-.898-.566l-.044-1.423a.64.64 0 0 0-.215-.456C.956 12.108 0 10.092 0 7.76m5.546-1.459-2.35 3.728c-.225.358.214.761.551.506l2.525-1.916a.48.48 0 0 1 .578-.002l1.869 1.402a1.2 1.2 0 0 0 1.735-.32l2.35-3.728c.226-.358-.214-.761-.551-.506L9.728 7.381a.48.48 0 0 1-.578.002L7.281 5.98a1.2 1.2 0 0 0-1.735.32z"
+                    />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-telephone text-green-500"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
+                    />
+                  </svg>
+                </div>
+                <div class="bg-gray-200 rounded-xl my-2 text-center">
+                  <div></div>
+                  <p class="text-[12px]">Social Login 身份認證</p>
+                </div>
+              </div>
+              <div class="p-3 bg-gray-200 rounded-2xl text-center">
+                <p class="bg-[#dcd5fd] text-[#7756cd] p-2 rounded-xl mb-2">
+                  已認證 HCP 醫事人員
+                </p>
+                <p class="text-[12px]">限定標籤進入頁面</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <iframe
+                  width="200"
+                  height="150"
+                  src="https://www.youtube.com/embed/VIDEO_ID?start=90"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  class="rounded-2xl p-2 bg-gray-200"
+                ></iframe>
+              </div>
+              <div>
+                <p class="text-[12px] font-bold p-2 text-center">
+                  影片觀看紀錄貼上標籤
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <div
+              class="w-[200px] bg-gray-200 rounded-2xl text-center p-3 ml-2 mt-2"
+            >
+              PDF 嵌入
+            </div>
+            <div
+              class="w-[200px] bg-gray-200 rounded-2xl text-center p-3 ml-2 mt-2"
+            >
+              所見即得編輯器
+            </div>
+          </div>
+        </div>
+        <div class="">
+          <div
+            class="w-[700px] bg-[#324053] text-white font-bold text-center p-2 rounded-xl m-2"
+          >
+            表單Form
+          </div>
+          <div class="flex justify-between px-2">
+            <div>
+              <div>填寫期限</div>
+              <div>總填寫次數</div>
+              <div>個人填寫次數</div>
+            </div>
+            <div>
+              <div class="flex justify-between">
+                <div class="mr-2">
+                  <div class="bg-gray-200 p-2 rounded-md text-center">
+                    <p>/(reg)ex/</p>
+                  </div>
+                  <!-- start -->
+                  <div class="bg-gray-200 p-2 rounded-md mt-2">
+                    <div class="flex space-x-1">
+                      <button
+                        v-for="i in 5"
+                        :key="i"
+                        type="button"
+                        @click="setRating(i)"
+                        class="focus:outline-none"
+                      >
+                        <!-- 實心星 (當前評分 ≥ i) -->
+                        <svg
+                          v-if="i <= rating"
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="w-6 h-6 text-yellow-400"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M9.049 2.927C9.349 2.03 10.651 2.03 10.951 2.927l.859 2.668a1 1 0 0 0 .95.69h2.8c.969 0 1.371 1.24.588 1.81l-2.267 1.647a1 1 0 0 0-.364 1.118l.859 2.668c.3.897-.755 1.64-1.54 1.118L10 12.347l-2.267 1.647c-.785.522-1.84-.221-1.54-1.118l.859-2.668a1 1 0 0 0-.364-1.118L4.421 8.095c-.783-.57-.38-1.81.588-1.81h2.8a1 1 0 0 0 .95-.69l.859-2.668z"
+                          />
+                        </svg>
+
+                        <!-- 空心星 -->
+                        <svg
+                          v-else
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="w-6 h-6 text-gray-300"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M11.049 2.927c.3-.897 1.702-.897 2.002 0l.859 2.668a1 1 0 00.95.69h2.8c.97 0 1.372 1.24.588 1.81l-2.267 1.647a1 1 0 00-.364 1.118l.859 2.668c.3.897-.755 1.64-1.54 1.118L12 12.347l-2.267 1.647c-.785.522-1.84-.221-1.54-1.118l.859-2.668a1 1 0 00-.364-1.118L4.421 8.095c-.783-.57-.38-1.81.588-1.81h2.8a1 1 0 00.95-.69l.859-2.668z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <!-- radio -->
+                  <div
+                    class="bg-gray-200 p-2 rounded-md mt-2 flex justify-between"
+                  >
+                    <input class="mr-1" type="radio" name="" id="" />
+                    <input class="mr-1" type="radio" name="" id="" />
+                    <input class="mr-1" type="radio" name="" id="" />
+                    <input class="mr-1" type="radio" name="" id="" />
+                    <input type="radio" name="" id="" />
+                  </div>
+                </div>
+                <div>2</div>
+              </div>
+              <div>css</div>
+              <!-- login btn -->
+              <div class="bg-gray-200 p-2 rounded-xl">
+                <div class="bg-white text-center flex justify-center">
+                  <button class="flex justify-center items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-line text-green-500"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M8 0c4.411 0 8 2.912 8 6.492 0 1.433-.555 2.723-1.715 3.994-1.678 1.932-5.431 4.285-6.285 4.645-.83.35-.734-.197-.696-.413l.003-.018.114-.685c.027-.204.055-.521-.026-.723-.09-.223-.444-.339-.704-.395C2.846 12.39 0 9.701 0 6.492 0 2.912 3.59 0 8 0M5.022 7.686H3.497V4.918a.156.156 0 0 0-.155-.156H2.78a.156.156 0 0 0-.156.156v3.486c0 .041.017.08.044.107v.001l.002.002.002.002a.15.15 0 0 0 .108.043h2.242c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157m.791-2.924a.156.156 0 0 0-.156.156v3.486c0 .086.07.155.156.155h.562c.086 0 .155-.07.155-.155V4.918a.156.156 0 0 0-.155-.156zm3.863 0a.156.156 0 0 0-.156.156v2.07L7.923 4.832l-.013-.015v-.001l-.01-.01-.003-.003-.011-.009h-.001L7.88 4.79l-.003-.002-.005-.003-.008-.005h-.002l-.003-.002-.01-.004-.004-.002-.01-.003h-.002l-.003-.001-.009-.002h-.006l-.003-.001h-.004l-.002-.001h-.574a.156.156 0 0 0-.156.155v3.486c0 .086.07.155.156.155h.56c.087 0 .157-.07.157-.155v-2.07l1.6 2.16a.2.2 0 0 0 .039.038l.001.001.01.006.004.002.008.004.007.003.005.002.01.003h.003a.2.2 0 0 0 .04.006h.56c.087 0 .157-.07.157-.155V4.918a.156.156 0 0 0-.156-.156zm3.815.717v-.56a.156.156 0 0 0-.155-.157h-2.242a.16.16 0 0 0-.108.044h-.001l-.001.002-.002.003a.16.16 0 0 0-.044.107v3.486c0 .041.017.08.044.107l.002.003.002.002a.16.16 0 0 0 .108.043h2.242c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157H11.81v-.589h1.525c.086 0 .155-.07.155-.156v-.56a.156.156 0 0 0-.155-.157H11.81v-.589h1.525c.086 0 .155-.07.155-.156Z"
+                      />
+                    </svg>
+                    <p>使用Line登入</p>
+                  </button>
+                </div>
+                <div class="my-2 bg-white text-center flex justify-center">
+                  <button class="flex justify-center items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-google"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"
+                      /></svg>使用Google登入
+                  </button>
+                </div>
+                <div class="my-2 bg-white text-center flex justify-center">
+                  <button class="flex justify-center items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-telephone text-green-500"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
+                      /></svg
+                    >使用手機號碼登入
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="flex bg-gray-200 p-2 mb-2">
+                <div>
+                  <p class="bg-[#dcd5fd] text-[#7756cd] p-2 rounded-xl mb-2">
+                    完成活動邀請
+                  </p>
+                  <p class="text-[12px]">限定標籤進入表單</p>
+                </div>
+                <div>
+                  <div>
+                    <input type="checkbox" name="" id="aa" class="m-2" /><label
+                      for="aa"
+                      >同意使用條款</label
+                    >
+                  </div>
+                  <div>
+                    <input type="checkbox" name="" id="bb" class="m-2" /><label
+                      for="bb"
+                      >同意隱私權條款</label
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="bg-gray-200 p-2 mb-2">
+                <div>
+                  <img
+                    class="w-[250px] h-[100px]"
+                    src="../../public//images/user/agent.jpeg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <input type="checkbox" name="" id="cc" class="m-2" /><label
+                    for="cc"
+                    >選擇題顯示圖片</label
+                  >
+                </div>
+              </div>
+              <div>上傳圖片檔案</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div
+          class="w-[1120px] bg-[#324053] text-white font-bold text-center p-2 rounded-xl m-2"
+        >
+          客服中心 Service Center
+        </div>
+        <div class="flex justify-between">
+          <div>1</div>
+          <div class="flex">
+            <div>1</div>
+            <div>2</div>
+          </div>
+          <div>3</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+input[type="checkbox"] {
+  accent-color: #2f4057;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "StarRating",
+  setup() {
+    // 當前評分
+    const rating = ref<number>(0);
+
+    /**
+     * 點擊星星時設定評分
+     * @param value 點擊的星星編號 (1~5)
+     */
+    function setRating(value: number) {
+      rating.value = value;
+    }
+
+    return {
+      rating,
+      setRating,
+    };
+  },
+});
+</script>
